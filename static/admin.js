64ae43893a2d2332daa3f34447af74ae0e8098ee
Dmitry Golovin <dima@golovin.in>
2016-11-19 01:14:15 +0200

    add admin view

---
+++ static/admin.js
@@ -0,0 +1,22 @@
+var ws;
+document.addEventListener('DOMContentLoaded', function() {
+  ws = new Wampy('/ws', { realm: 'AppRealm',
+    onConnect: function () { status('bg-green'); init(); },
+    onClose: function () { status('bg-red'); },
+    onError: function () { status('bg-red'); },
+    onReconnect: function () { status('bg-yellow'); },
+    onReconnectSuccess: function () { status('bg-green'); }
+  });
+  
+}, false);
+function status(cl) {
+  document.getElementById('status').className = cl;
+}
+function init() {
+  ws.subscribe('screens.online', function (data) { addmsg(data); });
+  ws.publish('screens.acknowledge');
+}
+function addmsg(msg) {
+  var box = document.getElementById('box');
+  box.innerHTML += msg + "<br />";
+}
