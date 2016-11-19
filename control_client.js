bea34905f9a708d603c241183e50ff31f42ac38f
voldemarich <voldemarich.dev@gmail.com>
2016-11-19 03:19:52 +0200

    Commit disrectories and js script

--- control_client.js
+++ control_client.js
@@ -2,3 +2,22 @@
  * Created by voldemarich on 19.11.2016.
  */
 
+conf = [
+    {
+        screen_name: 'Kokoustila-1',
+        id: 1,
+
+    }
+];
+
+$("#r1").loadTemplate("templates/screen_op_template.html",
+    {
+        screen_name: 'Kokoustila-kek',
+        write_text: 'fdfd',
+        write_file: 'topkek',
+        write_url: 'ddfdf',
+        write_blank: 'sifsjffs',
+        write_alert: '45tf',
+        screen_img: 'av.jpg'
+    }
+);
