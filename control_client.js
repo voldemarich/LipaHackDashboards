350eca4c69c786fc47a34066b62cf8fb819bbdac
voldemarich <voldemarich.dev@gmail.com>
2016-11-19 03:31:48 +0200

    js_loading

--- control_client.js
+++ control_client.js
@@ -5,19 +5,38 @@
 conf = [
     {
         screen_name: 'Kokoustila-1',
-        id: 1,
-
+        id: 1
+    },
+    {
+        screen_name: 'Kokoustila-2',
+        id: 2
+    },
+    {
+        screen_name: 'Sisäänkäynti-1',
+        id: 3
+    },
+    {
+        screen_name: 'Tehtaalla-4',
+        id: 4
     }
 ];
 
-$("#r1").loadTemplate("templates/screen_op_template.html",
-    {
-        screen_name: 'Kokoustila-kek',
-        write_text: 'fdfd',
-        write_file: 'topkek',
-        write_url: 'ddfdf',
-        write_blank: 'sifsjffs',
-        write_alert: '45tf',
-        screen_img: 'av.jpg'
+conf.forEach(function (x) {
+    row = "";
+    if (x.id % 2 == 0){
+        row = "#r2";
     }
-);
+    else row = "#r1";
+    $(row).loadTemplate("templates/screen_op_template.html",
+        {
+            screen_name: x.screen_name,
+            write_text: 'text_'+id,
+            write_file: 'file_'+id,
+            write_url: 'url_'+id,
+            write_blank: 'blank_'+id,
+            write_alert: 'alert_'+id,
+            screen_img: 'av.jpg'
+        }
+    );
+});
+
